4c922b3a51a485e3b49839d16732baaa1fdf59b8
Akovi Mensah <akovi66@gmail.com>
2021-05-24 23:24:47 -0500

    added playcontroller with adduser action

---
+++ Revachess.Client/Controllers/PlayController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using PizzaBox.Client.Models;
+using PizzaBox.Storage;
+using Revachess.Domain.Models;
+using Revachess.Storage;
+
+namespace Revachess.Client.Controllers
+{
+  [Route("[controller]")]
+  [EnableCors("public")]
+  public class PlayController : ControllerBase
+  {
+    private readonly UnitOfWork _unitOfWork;
+    public PlayController(UnitOfWork unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+    [HttpGet]
+    [HttpPost]
+    public IActionResult AddUser(User user)
+    {
+      
+      _unitOfWork.Users.Insert(user);
+      _unitOfWork.Save();
+
+      return Ok("User added");
+    }
+  }
+}
