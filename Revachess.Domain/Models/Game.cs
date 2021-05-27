ca855e87d33ba50c7bef17fc6b31c0beb8b726b6
Akovi Mensah <abmensah@wisc.edu>
2021-05-27 09:33:56 -0500

    added more properties to game class

--- Revachess.Domain/Models/Game.cs
+++ Revachess.Domain/Models/Game.cs
@@ -11,6 +11,22 @@ namespace Revachess.Domain.Models
     public User Player2 { get; set; }
     public User Winner { get; set; }
     public List<GameState> States { get; set; }
+    public bool Started = false;
+    public bool Ended = false;
+
+    public Game()
+    {
+      States = new List<GameState>();
+    }
+
+    public Game(string Player1Username, string Player2Username)
+    {
+      States = new List<GameState>();
+      Player1 = new User();
+      Player2 = new User();
+      Player1.UserName = Player1Username;
+      Player2.UserName = Player2Username;
+    }
 
   }
 }
